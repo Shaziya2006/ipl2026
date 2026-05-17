7c620d55a214cac4085516c23e8745dd17d527fa
Shaziya2006 <shaziyabanu5365@gmail.com>
2026-05-17 22:31:37 +0530

    program 4 completed

---
+++ prog4/prog4.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+// Structure with bitfields
+struct Bitfield
+{
+    unsigned int firstbit : 1;   // uses 1 bit
+    unsigned int secondbit : 2;  // uses 2 bits
+    unsigned int thirdbit : 3;   // uses 3 bits
+};
+
+int main()
+{
+    struct Bitfield a;
+
+    // Assign values
+    a.firstbit = 1;
+    a.secondbit = 3;
+    a.thirdbit = 5;
+
+    // Display values
+    printf("firstbit = %u\n", a.firstbit);
+    printf("secondbit = %u\n", a.secondbit);
+    printf("thirdbit = %u\n", a.thirdbit);
+
+    return 0;
+}
